29a00a09b4deffbcd1c3c50c85c10b0c169a5a46
tengrommel <wwwsousoucom@126.com>
2017-12-17 13:08:16 +0800

    student model

---
+++ REST/api_student.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"net/http"
+)
+
+func replyWithAllStudents(w http.ResponseWriter, db *StudentsDB) {
+	if db.students == nil {
+		json.NewEncoder(w).Encode([]Student{})
+	} else {
+		a := make([]Student, 0, len(db.students))
+		for _, s := range db.students{
+			a = append(a, s)
+		}
+		json.NewEncoder(w).Encode(a)
+	}
+}
+
+func replyWithStudent(w http.ResponseWriter, db *StudentsDB, id string)  {
+	s, ok := db.Get(id)
+	if !ok {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	json.NewEncoder(w).Encode(s)
+}
+
+func handlerStudent(w http.ResponseWriter, r *http.Request)  {
+	switch r.Method {
+	case "POST":
+		if r.Body == nil{
+			http.Error(w, "Student POST request must have JSON body", http.StatusBadRequest)
+			return
+		}
+		var s Student
+		err:=json.NewDecoder(r.Body).Decode(&s)
+		if err != nil{
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		// check if the student is new
+		_, ok :=db.Get(s.Id)
+		if ok {
+			// TODO find a better Error Code (HTTP Status)
+			http.Error(w, "Student already exists. Use PUT to modify.", http.StatusBadRequest)
+			return
+		}
+		// new student
+		db.Add(s)
+		fmt.Fprint(w,"ok") // 200 by default
+		return
+	case "GET": // student/<id>
+		http.Header.Add(w.Header(),"content-type", "application/json")
+		parts := strings.Split(r.URL.Path, "/")
+		if len(parts) != 3{
+			http.Error(w, "Malformed URL", http.StatusBadRequest)
+			return
+		}
+		if parts[2] == ""{
+			replyWithAllStudents(w, &db)
+		}else {
+			replyWithStudent(w, &db, parts[2])
+		}
+	default:
+		http.Error(w, "not implemented yet", http.StatusNotImplemented)
+		return
+	}
+}
--- REST/main.go
+++ REST/main.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 	"fmt"
 	"strings"
-	"encoding/json"
 )
 
 
@@ -12,7 +11,7 @@ var db StudentsDB
 
 func handlerHello(w http.ResponseWriter, r *http.Request)  {
 	parts := strings.Split(r.URL.Path, "/")
-	if (len(parts) != 4){
+	if len(parts) != 4{
 		status := 400
 		http.Error(w, http.StatusText(status), status)
 		return
@@ -22,48 +21,12 @@ func handlerHello(w http.ResponseWriter, r *http.Request)  {
 	fmt.Fprintf(w, "Hello %s %s!\n", name, parts[3])
 }
 
-func replyWithAllStudents(w http.ResponseWriter, db *StudentsDB) {
-	if db.students == nil {
-		json.NewEncoder(w).Encode([]Student{})
-	} else {
-		json.NewEncoder(w).Encode(db.students)
-	}
-}
-
-func replyWithStudent(w http.ResponseWriter, db *StudentsDB, id string)  {
-	s, ok := db.Get(id)
-	if !ok {
-		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-		return
-	}
-	json.NewEncoder(w).Encode(s)
-}
-
-func handlerStudent(w http.ResponseWriter, r *http.Request)  {
-	if r.Method == "POST"{
-		http.Error(w, "not implemented yet", http.StatusNotImplemented)
-		return
-	}
-	http.Header.Add(w.Header(),"content-type", "application/json")
-	parts := strings.Split(r.URL.Path, "/")
-
-	if len(parts) != 3{
-		return
-	}
-
-	if parts[2] == ""{
-		replyWithAllStudents(w, &db)
-		}else {
-			replyWithStudent(w, &db, parts[2])
-		}
-}
 
 
 
 func main() {
 	db = StudentsDB{}
 	db.Init()
-	db.Add(Student{"tom", 21, "id0"})
 	http.HandleFunc("/hello/", handlerHello)
 	http.HandleFunc("/student/", handlerStudent)
 	http.ListenAndServe(":8000", nil)
