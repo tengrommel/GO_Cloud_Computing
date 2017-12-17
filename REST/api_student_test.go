40b3b1e56eeb3be6527fb375f2bd104d78138353
tengrommel <wwwsousoucom@126.com>
2017-12-17 14:13:56 +0800

    getstudent

--- REST/api_student_test.go
+++ REST/api_student_test.go
@@ -88,7 +88,8 @@ func Test_handlerStudent_getAllStudents_Tom(t *testing.T)  {
 	// add new student
 	db = StudentsDB{}
 	db.Init()
-	db.Add(Student{"Tom", 21, "id0"})
+	testStudent := Student{"Tom", 21, "id0"}
+	db.Add(testStudent)
 	ts := httptest.NewServer(http.HandlerFunc(handlerStudent))
 	defer ts.Close()
 
@@ -107,5 +108,50 @@ func Test_handlerStudent_getAllStudents_Tom(t *testing.T)  {
 	}
 	if len(a) != 1 {
 		t.Errorf("Excpected array with one element, got %s", a)
+	}
+	if a[0].Name != testStudent.Name || a[0].Age != testStudent.Age || a[0].Id != testStudent.Id {
+		t.Errorf("Students do not match! Got: %s, Expected: %s\n", a[0], testStudent)
+	}
+}
+
+// GET /student/id0
+// empty array back
+func Test_handlerStudent_getStudents_Tom(t *testing.T)  {
+	// add new student
+	db = StudentsDB{}
+	db.Init()
+	testStudent := Student{"Tom", 21, "id0"}
+	db.Add(testStudent)
+	ts := httptest.NewServer(http.HandlerFunc(handlerStudent))
+	defer ts.Close()
+
+	// NotFound
+	resp, err := http.Get(ts.URL + "/student/id1")
+	if err != nil{
+		t.Errorf("Error making the GET request, %s", err)
+	}
+	if resp.StatusCode != http.StatusNotFound {
+		t.Errorf("Expected StatusCode %d, received %d", http.StatusNotFound, resp.StatusCode)
+		return
+	}
+
+
+	// Tom
+	resp, err = http.Get(ts.URL + "/student/id0")
+	if err != nil{
+		t.Errorf("Error making the GET request, %s", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected StatusCode %d, received %d", http.StatusOK, resp.StatusCode)
+		return
+	}
+	var a Student
+	err = json.NewDecoder(resp.Body).Decode(&a)
+	if err != nil{
+		t.Errorf("Error parsing the expected JSON body. Got error: %s", err)
+	}
+
+	if a.Name != testStudent.Name || a.Age != testStudent.Age || a.Id != testStudent.Id {
+		t.Errorf("Students do not match! Got: %s, Expected: %s\n", a, testStudent)
 	}
 }
