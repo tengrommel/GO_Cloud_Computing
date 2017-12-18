5b9867ca364068b879b1e1a99250e15864d3fc92
tengrommel <wwwsousoucom@126.com>
2017-12-18 23:11:47 +0800

    速度问题

--- Regular_Expressions/RegexClass/app.go
+++ Regular_Expressions/RegexClass/app.go
@@ -2,15 +2,37 @@ package main
 
 import (
 	"regexp"
+	"strings"
+	"time"
 	"fmt"
 )
 
+func tryString() {
+	input := "frexit, brexit, nexit, lexit"
+	strings.Contains(input, "frexit")
+	strings.Contains(input, "brexit")
+	strings.Contains(input, "grexit")
+	strings.Contains(input, "nexit")
+}
+
+func tryRegex()  {
+	pattern := "(br|n|gr|l)exit"
+	input := "frexit, brexit, nexit, lexit"
+	rgx, _:= regexp.Compile(pattern)
+	rgx.FindAllString(input, -1)
+}
+
+func track(start time.Time, name string)  {
+	duration := time.Since(start)
+	fmt.Printf("Duration for %s: %s", name, duration)
+}
+
 func main() {
-	pattern := "^[[:alnum:]]"
-	input := "fdafd"
-	res, err := regexp.MatchString(pattern, input)
-	if err != nil{
-		fmt.Println(err.Error())
+	defer track(time.Now(), "Evaluation")
+
+	for i:=0;i<10000;i++ {
+		//tryString()
+		tryRegex()
 	}
-	fmt.Println(res)
+
 }
