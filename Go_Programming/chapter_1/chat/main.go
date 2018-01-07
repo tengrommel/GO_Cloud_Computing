ff0c42d792c39cdaa7e02ceff3dce2a1b04efb47
tengrommel <wwwsousoucom@126.com>
2018-01-07 11:47:24 +0800

    websocket

---
+++ Go_Programming/chapter_1/chat/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"sync"
+	"text/template"
+	"github.com/tengrommel/GO_Cloud_Computing/Go_Programming/chapter_1/trace"
+	"net/http"
+	"path/filepath"
+	"flag"
+	"os"
+	"log"
+)
+
+// templ represents a single template
+type templateHandler struct {
+	once sync.Once
+	filename string
+	templ *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler)ServeHTTP(w http.ResponseWriter, r *http.Request)  {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "The addr of the application.")
+	flag.Parse() // parse the flags
+
+	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
+
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	// get the room going
+	go r.run()
+
+	// start the web server
+	log.Println("Starting web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil{
+		log.Fatal("ListenAndServe:", err)
+	}
+}
---
+++ Go_Programming/chapter_1/note.md
@@ -0,0 +1,3 @@
+# Tracing code to get a look under the hood
+
+> Tracer is the interface that describes an object capable of tracing events throughout code.
