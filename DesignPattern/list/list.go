3fbb74838e04c1cdff9bc4605441bec3dfbc8d40
tengrommel <wwwsousouccom@126.com>
2018-01-08 17:26:43 +0800

    list

---
+++ DesignPattern/list/list.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+func main() {
+	list := list.New()
+	list.PushBack(1)
+	list.PushBack(2)
+
+	fmt.Printf("len: %v\n", list.Len())
+	fmt.Printf("first: %#v\n", list.Front())
+	fmt.Printf("second: %#v\n", list.Front().Next())
+}
