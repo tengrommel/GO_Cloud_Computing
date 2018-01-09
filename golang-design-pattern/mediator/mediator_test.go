d7453bfdbe5020adbc9b422d72d214aaf0877464
tengrommel <wwwsousouccom@126.com>
2018-01-09 13:05:53 +0800

    中介

---
+++ golang-design-pattern/mediator/mediator_test.go
@@ -0,0 +1,31 @@
+package mediator
+
+import "testing"
+
+func TestMediator(t *testing.T)  {
+	mediator := GetMediatorInstance()
+	mediator.CD = &CDDriver{}
+	mediator.CPU = &CPU{}
+	mediator.Video = &VideoCard{}
+	mediator.Sound = &SoundCard{}
+
+	// Tiggle
+	mediator.CD.ReadData()
+
+	if mediator.CD.Data != "music,image"{
+		t.Fatalf("CD unexpect data %s", mediator.CD.Data)
+	}
+
+	if mediator.CPU.Sound != "music"{
+		t.Fatalf("CPU unexpect sound data %s", mediator.CPU.Sound)
+	}
+
+	if mediator.CPU.Video != "image"{
+		t.Fatalf("CPU unexpect data %s", mediator.Video.Data)
+	}
+
+	if mediator.Sound.Data != "music"{
+		t.Fatalf("SoundCard unexpected data %s", mediator.Sound.Data)
+	}
+
+}
