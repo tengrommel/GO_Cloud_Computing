b93aac0b187a4f8189b6777d2013981e08d4a0e6
tengrommel <wwwsousouccom@126.com>
2018-01-11 12:34:23 +0800

    info

--- Cloud_Networking/Chapter_3_Socket-level_Programming/GetHeadInfo/get_head_info.go
+++ Cloud_Networking/Chapter_3_Socket-level_Programming/GetHeadInfo/get_head_info.go
@@ -39,3 +39,19 @@ func checkError(err error) {
 		os.Exit(1)
 	}
 }
+/*
+The first point to note is the almost excessive amount of error checking that is going on.
+This is normal for networking programs:
+	the opportunities for failure are substantially greater than for standalone programs.
+	Hardware may fail on the client, the server, or on any of the routers and switches in the middle.
+	communication may be blocked by a firewall;
+	timeouts may occur due to network load;
+	the server may crash while the client is talking to it.
+
+The following checks are performed:
+	There may be syntax errors in the address specified
+	The attempt to connect to the remote service may fail. For example, the service requested might not be running, or there may be no such host connected to the network
+	Although a connection has been established, writes to the service might fail if the connection has died suddenly, or the network times out
+	Similarly, the reads might fail
+
+ */
