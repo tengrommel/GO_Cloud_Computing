82c518677a7c2a0613a8a5754de8e2ecfabf6db5
tengrommel <wwwsousouccom@126.com>
2018-01-31 09:55:54 +0800

    增加忽略限制

---
+++ RabbitMQ/Topics/emit_log_topic/app.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string)  {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		"logs_topic",
+		"topic",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to declare an exchang")
+
+	body := bodyFrom(os.Args)
+	err = ch.Publish(
+		"logs_topic",
+		severityFrom(os.Args),
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body: []byte(body),
+		})
+}
+
+func bodyFrom(args []string) string {
+	var s string
+	if (len(args) < 3) || os.Args[2] == "" {
+		s = "hello"
+	} else {
+		s = strings.Join(args[2:], " ")
+	}
+	return s
+}
+
+func severityFrom(args []string) string {
+	var s string
+	if (len(args) < 2) || os.Args[1] == "" {
+		s = "anonymous.info"
+	} else {
+		s = os.Args[1]
+	}
+	return s
+}
