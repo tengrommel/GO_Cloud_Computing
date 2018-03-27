735a9cf582e25adea459f67110b9b57892338d01
tengrommel <wwwsousoucom@126.com>
2018-03-27 23:14:01 +0800

    更好的打印

--- JOB/ch06/time/main.go
+++ JOB/ch06/time/main.go
@@ -5,10 +5,9 @@ import (
 	"time"
 )
 
-func main()  {
-	// 代码的可读性更高
+func main() {
 	var n time.Duration
-	n = 2 * time.Millisecond
-	time.Sleep(n)
-	fmt.Println(time.Now())
+	n = time.Hour
+	// 更好的打印
+	fmt.Println(n.String())
 }
