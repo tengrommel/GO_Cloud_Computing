a9742b7c443fc0ebca2eaadeda837f25b1cadc57
tengrommel <wwwsousoucom@126.com>
2018-03-27 23:16:28 +0800

    更好的可读性

--- JOB/ch06/time/main.go
+++ JOB/ch06/time/main.go
@@ -9,5 +9,9 @@ func main() {
 	var n time.Duration
 	n = time.Hour
 	// 更好的打印
+	n = 3 * time.Hour + 30 * time.Minute
+	fmt.Println(int64(n))
 	fmt.Println(n.String())
+	fmt.Println(n.Seconds())
+	fmt.Println(n.Minutes())
 }
