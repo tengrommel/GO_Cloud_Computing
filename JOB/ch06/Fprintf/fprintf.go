7a88e0fa957d9827cace2f414e7361580a00ba9a
tengrommel <wwwsousoucom@126.com>
2018-03-28 10:42:42 +0800

    pointer

--- JOB/ch06/Fprintf/fprintf.go
+++ JOB/ch06/Fprintf/fprintf.go
@@ -1,21 +1,70 @@
 package main
 
 import (
-	"os"
-	"github.com/prometheus/common/log"
 	"fmt"
 	"net/http"
+	"math"
 )
 
 func handle(w http.ResponseWriter, r *http.Request)  {
 	fmt.Fprintf(w, "hello\n")
 }
 
-func main() {
-	f, err := os.Create("a.txt")
-	if err !=nil{
-		log.Fatal(err)
+// 名字： IInstance
+// 方法： Instance() float64
+type IInstance interface {
+	Instance() float64
+}
+
+type Point struct {
+	X float64
+	Y float64
+}
+
+type Path []*Point
+
+func (p Path)Instance() float64 {
+	return 0.1
+}
+
+func (p Point)Distance2Point(q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y- q.X)
+}
+
+func (p Point)Distance() float64 {
+	return math.Hypot(p.X, p.Y)
+}
+
+
+func (p Path)Distance() float64{
+	var sum float64
+	for i:=0;i<len(p)-1;i++{
+		sum += p[i].Distance2Point(*p[i+1])
 	}
-	defer f.Close()
-	fmt.Fprintf(f, "hello\n")
+	return sum
+}
+
+type IDistance interface {
+		Distance() float64
+}
+
+func main() {
+	var path Path
+	path = make([]*Point, 3)
+	p1 := &Point{X:1, Y:2}
+	p2 := &Point{X:3, Y:4}
+	p3 := &Point{X:5, Y:6}
+	path[0] = p1
+	path[1] = p2
+	path[2] = p3
+
+	var i IDistance
+	i = p1
+	fmt.Println(i.Distance())
+	i = p2
+	fmt.Println(i.Distance())
+	i = path
+	fmt.Println(i.Distance())
+	fmt.Println(path)
+	fmt.Println(p1)
 }
--- JOB/ch06/ok.md
+++ JOB/ch06/ok.md
@@ -4,3 +4,9 @@
 >为什么需要接口
 
 归一化处理
+
+定义接口实现规范
+
+    type Writer inteface {
+        Write(p []byte) (n int, err error)
+    }
