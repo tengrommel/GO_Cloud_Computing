04b12eb51bb981a4d754fbf622758f0d7cfd6dac
tengrommel <wwwsousoucom@126.com>
2018-03-30 12:24:49 +0800

    回调

---
+++ JOB/example/func_order/11函数类型/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func minus(a, b int) int {
+	return a - b
+}
+
+// 函数也是一种数据类型， 通过type给一个函数类型起名
+// FuncType它是一个函数类型
+type FuncType func(int, int) int // 没有函数名字，没有{}
+
+func main() {
+	var result int
+
+	result = Add(1, 1) // 传统调用方法
+	fmt.Println(result)
+	// 声明一个函数类型的变量，变量名叫fTest
+	var fTest FuncType
+	fTest = Add // 是变量就可以赋值
+	result = fTest(10, 20) // 等价于Add(10, 20)
+	fmt.Println("result = ", result)
+	fTest = minus
+	result = fTest(10, 5)
+	fmt.Println("result = ", result)
+}
